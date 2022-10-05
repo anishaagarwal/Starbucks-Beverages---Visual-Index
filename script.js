0b9ec69777bd10a570b0d0514d5fd9cb217f87c7
Glitch (minimal-site-starter) <none>
2022-10-05 00:34:15 +0000

    🐏🐗 Checkpoint ./script.js:48159690/22 ./index.html:48159690/88

--- script.js
+++ script.js
@@ -7,15 +7,16 @@ const val = "this is a string!"
 const jobs = ["teacher", "nurse", "driver", "Uber", "Lyft"]
 
 console.log(jobs)
+
 /////////////////////////////////////////
 // SOME LINES TO USE FOR THE CHALLENGE//
 ////////////////////////////////////////
 
 // this line of code will create a new HTML p element
-let element = document.createElement("p");
+let el = document.createElement("p");
 
 // this line of code will assign text inside of the element created above
-element.innerHTML = "some text";
+el.innerHTML = "some text";
 
 // this code will grab the element with id "answer" and add something to it
-document.getElementById("answer").appendChild(element)
+document.getElementById("answer").appendChild(el)
