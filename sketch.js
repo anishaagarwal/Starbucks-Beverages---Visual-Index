3c457fff3d896bf70f20527e2eec09f4c62f4902
Glitch (minimal-site-starter) <none>
2022-10-11 23:25:39 +0000

    🌿💘 Checkpoint ./sketch.js:48159690/673

--- sketch.js
+++ sketch.js
@@ -1,18 +1,24 @@
-const x = 120;
+const numArray = [2, 1, 5, 4, 8, 9];
 
 // happens only once!
 function setup() {
   createCanvas(500, 400);
   background(255, 255, 255);
   stroke(30, 30, 220)
-  strokeWeight(3)
+  strokeWeight(0)
   
 }
 
 // happens forever! unless call noLoop() function inside
 function draw() {
-  fill(255,0,0)
-  ellipse(x,250,80)
-  fill(20,40,100)
-  ellipse(x, 15, 20)
+  let x = 159;
+  let y = 80;
+  let m = map(max(numArray), 0, 10, 0, 10);
+
+  // Draw the Maximum value in the array.
+  textSize(32);
+  text('Max is: '+max(numArray), x, y)  
+  
+  fill(30,20,100)
+  ellipse(x, 200, m*10)
 }
