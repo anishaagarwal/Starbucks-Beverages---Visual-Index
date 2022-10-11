740f0046fa9715a6024490947072dc14ec9a8549
Glitch (minimal-site-starter) <none>
2022-10-11 23:00:12 +0000

    🌮🐡 Checkpoint ./sketch.js:48159690/151

--- sketch.js
+++ sketch.js
@@ -6,11 +6,13 @@ function setup() {
   background(255, 255, 255);
   stroke(30, 30, 220)
   strokeWeight(3)
-  fill(255,0,0)
-  ellipse(x,250,80)
+  
 }
 
 // happens forever! unless call noLoop() function inside
 function draw() {
-  
+  fill(255,0,0)
+  ellipse(x,250,80)
+  fill(20,40,100)
+  ellipse(x, 15, 20)
 }
