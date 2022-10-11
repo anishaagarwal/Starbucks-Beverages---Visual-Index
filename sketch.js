9885aa217080588062e29690573a29385727031b
Glitch (minimal-site-starter) <none>
2022-10-11 22:49:11 +0000

    ⏱👭 Checkpoint ./sketch.js:48159690/217

--- sketch.js
+++ sketch.js
@@ -1,16 +1,16 @@
-
-let x = 50;
-let speed = 5
+const x = 120;
 
 // happens only once!
 function setup() {
   createCanvas(500, 400);
-  background(205);
-  stroke(30)
+  background(255, 255, 255);
+  stroke(30, 30, 220)
+  strokeWeight(3)
+  fill(255,0,0)
+  ellipse(x,250,80)
 }
 
 // happens forever! unless call noLoop() function inside
 function draw() {
-  fill(255,0,0)
-  ellipse(x,50,30)
+  
 }
