cecd4f8d598ffecebbac9f9e157680056e626271
Glitch (minimal-site-starter) <none>
2022-10-18 17:22:47 +0000

    🍮👣 Checkpoint ./style.css:48159690/2 ./sketch.js:48159690/133

--- sketch.js
+++ sketch.js
@@ -1,27 +1,27 @@
 // add more key/value pairs to your object from the questions
 const answers = {
-  'fromMass': "yes",
+  'fromMass': "no",
 }
 
 // happens only once!
 function setup() {
   createCanvas(500, 500);
   background(255, 255, 255);
-  stroke(30, 30, 220)
 }
 
 // happens forever! unless call noLoop() function inside
 function draw() {
-  let x = 159;
-  let y = 80;
+  
+  stroke(10)
+  strokeWeight(3)
   
   if (answers.fromMass == "no") {
-    fill(30,20,100)  
+    fill(30,220,200)  
   } else {
     fill(200)
   }
   
-  ellipse(x, 200, 10)
+  ellipse(100, 200, 40)
   
   
   // this function prevents p5 from looping over and over, since we don't need animation
