593fc04875e480048dbb32da72a56d07ada38e25
Glitch (minimal-site-starter) <none>
2022-10-19 14:44:08 +0000

    💐🤘 Checkpoint ./sketch.js:48159690/678

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-
+// initialize the table as an empty variable
 let table;
 
 // preload happens only once, and is required to happen before setup
@@ -22,22 +22,26 @@ function setup() {
 // all drawing happens here, but only once because of the noLoop() at the end
 function draw() {
   stroke(10)
-  strokeWeight(1)
-  console.log(table)
+  strokeWeight(1)  
   
+  // we can access how many rows and columns using getColumnCount() or getRowCount() method on table
   console.log('Num cols: '+table.getColumnCount())
   console.log('Num rows: '+ table.getRowCount())
 
-  // loop through the table for every row in the CSV
+  // loop through the table for every row in the CSV using getRowCount()
   for (let row = 0; row < table.getRowCount(); row++) {
-    // for this row, store the value for the cell in column 6
+    // getNum() method takes two arguments: first is the row index, and second is the column index
+    // if we pass in 5, then we are grabbing data for this row from column number 6
+    // you can change the number to access different columns in the CSV
     let value = table.getNum(row,5)
     
     // map this value onto a range of pixels using the min/max of the dataset
     let x = map(value,1970,2019,0,width)
     
+    // this variable is hardcoded, but you could also use another map() to add more data!
     let y = height/2;
        
+    // draw a circle using the x and y variables above
     ellipse(x, y, 5) 
   }
   
