07f9d60a7dbac4d2964796e0b89e09c04593bd2d
Glitch (minimal-site-starter) <none>
2022-10-19 14:32:47 +0000

    💘⛰ Checkpoint ./sketch.js:48159690/313

--- sketch.js
+++ sketch.js
@@ -4,21 +4,22 @@ let table;
 // preload happens only once, and is required to happen before setup
 // if we load data here, it ensures we don't draw until data is ready!
 function preload() {
-  //my table is comma separated value "csv"
-  //and has a header specifying the columns labels
-  table = loadTable('./data/nobel_prizes_50yrs.csv', 'csv', 'header');
+  // my table is comma separated value "csv"
+  // and has a header specifying the columns labels
+  table = loadTable('./nobel_prizes_50yrs.csv', 'csv', 'header');
 }
 
-// happens only once!
+// happens only once, after preload() triggers
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(600, 600);
   background(255, 255, 255);
   
   // console out the table and look for the columns we are interested in
+  // using the developer tools of the browser (Command+Option+i on a Mac or "Inspect" on right click)
   console.log(table)
 }
 
-// happens forever! unless call noLoop() function inside
+// all drawing happens here, but only once because of the noLoop() at the end
 function draw() {
   stroke(10)
   strokeWeight(1)
@@ -35,12 +36,10 @@ function draw() {
     // map this value onto a range of pixels using the min/max of the dataset
     let x = map(value,1970,2019,0,width)
     
-    
     let y = height/2;
        
-      ellipse(x, y, 5) 
+    ellipse(x, y, 5) 
   }
-  
   
   // this function prevents p5 from looping over and over, since we don't need animation
   noLoop()
