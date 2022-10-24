2f7f33967bf347e9a01fc11e678802331f31d94e
Glitch (minimal-site-starter) <none>
2022-10-24 19:21:32 +0000

    🏏🌻 Checkpoint ./sketch.js:48706768/2967

--- sketch.js
+++ sketch.js
@@ -1,6 +1,12 @@
 let table;
 let numRows;
 let numCols;
+let answers = {
+  Calories: "",
+  Caffeine: "",
+  Carbs: "",
+  Sugar: "",
+};
 
 function preload() {
   table = loadTable("./Starbucks_coffee.csv", "csv", "header");
@@ -9,19 +15,21 @@ function preload() {
 function setup() {
   createCanvas(600, 600);
   background(255, 255, 255);
-  numRows=table.getRowCount();
-  numCols=table.getColumnCount()
+  numRows = table.getRowCount();
+  numCols = table.getColumnCount();
 }
 
 // all drawing happens here, but only once because of the noLoop() at the end
 function draw() {
   stroke(10);
   strokeWeight(1);
-  for (let row=0; row<numRows;row++){
-    
-  }
+  
+}
 
- 
+function allValues(){
+  
+}
+
   //   for (let row = 0; row < table.getRowCount(); row++) {
   //     // getNum() method takes two arguments: first is the row index, and second is the column index
   //     // if we pass in 5, then we are grabbing data for this row from column number 6
@@ -40,4 +48,4 @@ function draw() {
 
   //   // this function prevents p5 from looping over and over, since we don't need animation
   //   noLoop()
-}
+
