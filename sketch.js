108148c71d30b3c2598327d4d871993651018a24
Glitch (minimal-site-starter) <none>
2022-10-24 19:32:55 +0000

    🌗🌲 Checkpoint ./sketch.js:48706768/3778

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ let table;
 let numRows;
 let numCols;
 let answers = {
-  Calories: "",
+  Calories: "100",
   Caffeine: "",
   Carbs: "",
   Sugar: "",
@@ -13,7 +13,7 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(200, 300);
   background(255, 255, 255);
   numRows = table.getRowCount();
   numCols = table.getColumnCount();
@@ -21,31 +21,31 @@ function setup() {
 
 // all drawing happens here, but only once because of the noLoop() at the end
 function draw() {
-  stroke(10);
-  strokeWeight(1);
-  
+
+ let a = answers.Calories;
+  let mappeda = map(a, 0, 280, 300, 0);
+  stroke("#C58930");
+  strokeWeight(5);
+  line(20, 0, 20, mappeda);
 }
 
-function allValues(){
-  
-}
 
-  //   for (let row = 0; row < table.getRowCount(); row++) {
-  //     // getNum() method takes two arguments: first is the row index, and second is the column index
-  //     // if we pass in 5, then we are grabbing data for this row from column number 6
-  //     // you can change the number to access different columns in the CSV
-  //     let value = table.getNum(row,5)
 
-  //     // map this value onto a range of pixels using the min/max of the dataset
-  //     let x = map(value,1970,2019,0,width)
+//   for (let row = 0; row < table.getRowCount(); row++) {
+//     // getNum() method takes two arguments: first is the row index, and second is the column index
+//     // if we pass in 5, then we are grabbing data for this row from column number 6
+//     // you can change the number to access different columns in the CSV
+//     let value = table.getNum(row,5)
 
-  //     // this variable is hardcoded, but you could also use another map() to add more data!
-  //     let y = height/2;
+//     // map this value onto a range of pixels using the min/max of the dataset
+//     let x = map(value,1970,2019,0,width)
 
-  //     // draw a circle using the x and y variables above
-  //     ellipse(x, y, 5)
-  //   }
+//     // this variable is hardcoded, but you could also use another map() to add more data!
+//     let y = height/2;
 
-  //   // this function prevents p5 from looping over and over, since we don't need animation
-  //   noLoop()
+//     // draw a circle using the x and y variables above
+//     ellipse(x, y, 5)
+//   }
 
+//   // this function prevents p5 from looping over and over, since we don't need animation
+//   noLoop()
