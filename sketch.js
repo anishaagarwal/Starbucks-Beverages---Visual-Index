837c229cbe80fe805567d49b1d7cb9fc44a3f8a8
Glitch (minimal-site-starter) <none>
2022-10-24 19:09:38 +0000

    🍎🚁 Checkpoint ./sketch.js:48706768/719

--- sketch.js
+++ sketch.js
@@ -1,4 +1,6 @@
 let table;
+let numRows;
+let numCols;
 
 function preload() {
   table = loadTable("./Starbucks_coffee.csv", "csv", "header");
@@ -7,18 +9,19 @@ function preload() {
 function setup() {
   createCanvas(600, 600);
   background(255, 255, 255);
-  print(table.getRowCount() + "total rows ");
-  print(table.getColumnCount() + "total columns");
+  numRows=table.getRowCount();
+  numCols=table.getColumnCount()
 }
 
 // all drawing happens here, but only once because of the noLoop() at the end
 function draw() {
   stroke(10);
   strokeWeight(1);
-  console.log("Num cols: " + table.getColumnCount());
-  console.log("Num rows: " + table.getRowCount());
+  for (let row=0; row<numRows;row++){
+    
+  }
 
-  //   // loop through the table for every row in the CSV using getRowCount()
+ 
   //   for (let row = 0; row < table.getRowCount(); row++) {
   //     // getNum() method takes two arguments: first is the row index, and second is the column index
   //     // if we pass in 5, then we are grabbing data for this row from column number 6
