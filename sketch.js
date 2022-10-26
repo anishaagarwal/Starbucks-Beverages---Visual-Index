bf1d9821017500209f63f37a8ac8768211f5e2cb
Glitch (minimal-site-starter) <none>
2022-10-26 16:48:34 +0000

    🚅🏙 Checkpoint ./sketch.js:48159690/438:48706768/9307

--- sketch.js
+++ sketch.js
@@ -13,16 +13,31 @@ function setup() {
   numCols = table.getColumnCount();
   console.log(numRows, numCols);
 
+  let counter = 0;
   for (let r = 0; r < 6; r++) {
+    co
     for (let c = 0; c < 4; c++) {
       rect(r * 200, c * 300, 200, 300);
+      //console.log(c)
     }
   }
 }
 
 function draw() {
-  beverage(0, 0);
-   beverage(1, 0);
+  for (let i = 0; i < numRows; i++) {
+    // console.log(table.rows[i].obj['Caffeine (mg)']);
+    // let x = r * 200;
+    // let y = c * 300;
+    //console.log(i)
+    beverage(0, 0, i);
+    
+    // stroke("#C58930");
+    // strokeWeight(10);
+    // line(x + 30, y + 300, x + 30, mapCaf);
+  }
+
+  // FOR LOOP ITERATING OVER DATA GOES HERE
+
   // noFill();
   // for (let x = 50; x < width; x += 50) {
   //   for (let y = 50; y < height; y += 50) {
@@ -32,17 +47,24 @@ function draw() {
 }
 
 //creating a function for all values to be plotted
-function beverage(r, c) {
-  for (let i = 0; i < numRows; i++) {
-    // console.log(table.rows[i].obj['Caffeine (mg)']);
-    let x = r * 200;
-    let y = c * 300;
-    let caffeine = table.getNum(i, 3);
+function beverage(r, c, index) {
+  let x = r * 200;
+  let y = c * 300;
+  let caffeine = table.getNum(index, 3);
     let mapCaf = map(caffeine, 0, 300, y + 300, y);
-    stroke("#C58930");
-    strokeWeight(10);
-    line(x + 80, y + 300, x + 30, mapCaf);
-  }
+  stroke("#C58930");
+  strokeWeight(10);
+  line(x + 30, y + 300, x + 30, mapCaf);
+  // for (let i = 0; i < numRows; i++) {
+  //   // console.log(table.rows[i].obj['Caffeine (mg)']);
+  //   let x = r * 200;
+  //   let y = c * 300;
+  //   let caffeine = table.getNum(i, 3);
+  //   let mapCaf = map(caffeine, 0, 300, y + 300, y);
+  //   stroke("#C58930");
+  //   strokeWeight(10);
+  //   line(x + 30, y + 300, x + 30, mapCaf);
+  // }
 }
 
 //   let a = answers.Calories;
